cb5fed8809dcb6489b78a0b6d890025f3553d143
JahidulIslam1 <jahid.fj@gmail.com>
2022-05-30 20:08:34 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const { BrowserWindow, app } = require("electron");
+
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow();
+    mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
+
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow();
+    }
+})
